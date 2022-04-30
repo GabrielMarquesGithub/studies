5681cc284bad79d0973f911c05a754095419b649
Gabriel Marques Magalhães <gabrielmagalhaes777@gmail.com>
2022-04-30 16:30:42 -0300

    Estudos sobre JS

---
+++ Estudo JavaScript/JS - basic/meu-script.js
@@ -0,0 +1,18 @@
+document.getElementById('nome').value = 'oi'
+
+
+//string
+var texto = 'esse texto é uma string'
+
+//number
+var numeroInt = -7
+var numeroFloat = 1111.23
+
+//boolean
+var test = true
+
+
+//---------- Comandos ---------- 
+//alert(texto)
+//document.write(texto)
+//console.log(numeroInt)
